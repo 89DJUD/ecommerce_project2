8222931f8308e5390899317e858c920c8613a3b6
89DJUD <rabab.belya03@gmail.com>
2026-04-22 09:49:37 +0100

    Initial commit

---
+++ ecommerce/ecommerce/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='product_list', permanent=False)),
+    path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ ecommerce/products/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, 'products_list.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'product_detail.html', {'product': product})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'category_list.html', {'categories': categories})
+
+def category_detail(request, pk):
+    category = get_object_or_404(Category, id=pk)
+    products = category.products.all()
+    return render(request, 'category_detail.html', {
+        'category': category,
+        'products': products
+    })
